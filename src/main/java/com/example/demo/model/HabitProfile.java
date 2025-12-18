7b180ab4c0b1c994aaf872feffc3f0ce2f8f9938
bala-amypo <bala@amypo.in>
2025-12-18 08:55:13 +0000

    User commit

--- src/main/java/com/example/demo/model/HabitProfile.java
+++ src/main/java/com/example/demo/model/HabitProfile.java
@@ -4,7 +4,9 @@ public class HabitProfile{
     @ID
     private Long id;
     private Long studentId;
+    private String sleepSchedule;
     @Min(100)
     private int studyHoursPerDay;
+    private String clean
     private LocalDateTime updatedAt;
 }
