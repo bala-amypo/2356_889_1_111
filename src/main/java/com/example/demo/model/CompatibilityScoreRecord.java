c00fd26556b529a8281541f9d30c28f0028e2eff
bala-amypo <bala@amypo.in>
2025-12-18 09:15:14 +0000

    User commit

--- src/main/java/com/example/demo/model/CompatibilityScoreRecord.java
+++ src/main/java/com/example/demo/model/CompatibilityScoreRecord.java
@@ -1,1 +1,72 @@
-CompatibilityScoreRecord
+
+package com.example.project.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.PositiveOrZero;
+
+public class CompatibilityScoreRecord {
+    @Id
+    private Long id;
+    private Long studentAId;
+    private Long studentBId;
+    @PositiveOrZero
+    private Double score;
+    private String compatibilityLevel;
+    private LocalDateTime computedAt;
+    private String detailsJson; 
+    public CompatibilityScoreRecord(Long id, Long studentAId, Long studentBId, @PositiveOrZero Double score,
+            String compatibilityLevel, LocalDateTime computedAt, String detailsJson) {
+        this.id = id;
+        this.studentAId = studentAId;
+        this.studentBId = studentBId;
+        this.score = score;
+        this.compatibilityLevel = compatibilityLevel;
+        this.computedAt = computedAt;
+        this.detailsJson = detailsJson;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getStudentAId() {
+        return studentAId;
+    }
+    public void setStudentAId(Long studentAId) {
+        this.studentAId = studentAId;
+    }
+    public Long getStudentBId() {
+        return studentBId;
+    }
+    public void setStudentBId(Long studentBId) {
+        this.studentBId = studentBId;
+    }
+    public Double getScore() {
+        return score;
+    }
+    public void setScore(Double score) {
+        this.score = score;
+    }
+    public String getCompatibilityLevel() {
+        return compatibilityLevel;
+    }
+    public void setCompatibilityLevel(String compatibilityLevel) {
+        this.compatibilityLevel = compatibilityLevel;
+    }
+    public LocalDateTime getComputedAt() {
+        return computedAt;
+    }
+    public void setComputedAt(LocalDateTime computedAt) {
+        this.computedAt = computedAt;
+    }
+    public String getDetailsJson() {
+        return detailsJson;
+    }
+    public void setDetailsJson(String detailsJson) {
+        this.detailsJson = detailsJson;
+    }
+
+    
+}
