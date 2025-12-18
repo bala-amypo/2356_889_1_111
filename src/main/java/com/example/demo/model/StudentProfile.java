52ddfc739fa993cff1be8579f21fe49540de8013
bala-amypo <bala@amypo.in>
2025-12-18 08:30:14 +0000

    User commit

--- src/main/java/com/example/demo/model/StudentProfile.java
+++ src/main/java/com/example/demo/model/StudentProfile.java
@@ -1,5 +1,11 @@
 package.com.example.demo.model;
 
 public class StudentProfile{
-    
+    private Long id;
+    private String studentId;
+    private String fullName;
+    private String email;
+    private String department;
+    private int 
+
 }
