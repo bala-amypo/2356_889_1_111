21a19bed446e49d91d611248d3e8d257c12e1cef
bala-amypo <bala@amypo.in>
2025-12-18 08:40:14 +0000

    User commit

--- src/main/java/com/example/demo/model/StudentProfile.java
+++ src/main/java/com/example/demo/model/StudentProfile.java
@@ -1,6 +1,6 @@
 package.com.example.demo.model;
 
-import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 
 public class StudentProfile{
     @Id
@@ -12,5 +12,64 @@ public class StudentProfile{
     private int yearlevel;
     private boolean active;
     private LocalDateTime createdAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public StudentProfile(Long id, String studentId, String fullName, String email, String department, int yearlevel,
+            boolean active, LocalDateTime createdAt) {
+        this.id = id;
+        this.studentId = studentId;
+        this.fullName = fullName;
+        this.email = email;
+        this.department = department;
+        this.yearlevel = yearlevel;
+        this.active = active;
+        this.createdAt = createdAt;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    public int getYearlevel() {
+        return yearlevel;
+    }
+    public void setYearlevel(int yearlevel) {
+        this.yearlevel = yearlevel;
+    }
+    public boolean isActive() {
+        return active;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
 }
