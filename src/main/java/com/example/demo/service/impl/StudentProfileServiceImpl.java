bb34196b821494246d47332b1f84962a90660d0f
bala-amypo <bala@amypo.in>
2025-12-21 15:30:43 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentProfileServiceImpl.java
@@ -21,8 +21,7 @@ public class StudentProfileServiceImpl
 
     @Override
     public StudentProfile createStudent(StudentProfile profile) {
-        repo.findByStudentId(profile.getStudentId())
-            .ifPresent(s -> {
+        repo.findByStudentId(profile.getStudentId()).ifPresent(s -> {
                 throw new ResourceNotFoundException("studentId exists");
             });
         return repo.save(profile);
@@ -30,9 +29,8 @@ public class StudentProfileServiceImpl
 
     @Override
     public StudentProfile getStudentById(Long id) {
-        return repo.findById(id)
-                .orElseThrow(() ->
-                    new ResourceNotFoundException("not found"));
+        return repo.findById(id).orElseThrow(() ->
+            new ResourceNotFoundException("not found"));
     }
 
     @Override
@@ -42,16 +40,14 @@ public class StudentProfileServiceImpl
 
     @Override
     public StudentProfile findByStudentId(String studentId) {
-        return repo.findByStudentId(studentId)
-                .orElseThrow(() ->
-                    new ResourceNotFoundException("not found"));
+        return repo.findByStudentId(studentId).orElseThrow(() ->
+            new ResourceNotFoundException("not found"));
     }
 
     @Override
     public StudentProfile updateStudentStatus(Long id, boolean active) {
-        StudentProfile student = repo.findById(id)
-                .orElseThrow(() ->
-                    new ResourceNotFoundException("not found"));
+        StudentProfile student = repo.findById(id).orElseThrow(() ->
+        new ResourceNotFoundException("not found"));
         student.setActive(active);
         return repo.save(student);
     }
