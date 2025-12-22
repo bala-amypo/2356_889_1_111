e81fed3e5157e0eecc417353fc876407d8e93371
bala-amypo <bala@amypo.in>
2025-12-22 16:59:41 +0000

    User commit

--- src/main/java/com/example/demo/repository/CompatibilityScoreRecordRepository.java
+++ src/main/java/com/example/demo/repository/CompatibilityScoreRecordRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface CompatibilityScoreRecordRepository
-        extends JpaRepository<CompatibilityScoreRecord, Long> {
+public interface CompatibilityScoreRecordRepository extends JpaRepository<CompatibilityScoreRecord, Long> {
 
     Optional<CompatibilityScoreRecord>
     findByStudentAIdAndStudentBId(Long studentAId, Long studentBId);
