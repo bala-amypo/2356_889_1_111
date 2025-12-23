6630522c038cdf67b88dd2b7b85047b0ad7c6d27
bala-amypo <bala@amypo.in>
2025-12-23 15:03:50 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/HabitProfileDto.java
@@ -0,0 +1,66 @@
+package com.example.demo.dto;
+
+import com.example.demo.model.CleanlinessLevel;
+import com.example.demo.model.NoiseTolerance;
+import com.example.demo.model.SleepSchedule;
+import com.example.demo.model.SocialPreference;
+
+public class HabitProfileDto {
+
+    private Long studentId;
+    private SleepSchedule sleepSchedule;
+    private Integer studyHoursPerDay;
+    private CleanlinessLevel cleanlinessLevel;
+    private NoiseTolerance noiseTolerance;
+    private SocialPreference socialPreference;
+
+    public HabitProfileDto() {}
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public SleepSchedule getSleepSchedule() {
+        return sleepSchedule;
+    }
+
+    public void setSleepSchedule(SleepSchedule sleepSchedule) {
+        this.sleepSchedule = sleepSchedule;
+    }
+
+    public Integer getStudyHoursPerDay() {
+        return studyHoursPerDay;
+    }
+
+    public void setStudyHoursPerDay(Integer studyHoursPerDay) {
+        this.studyHoursPerDay = studyHoursPerDay;
+    }
+
+    public CleanlinessLevel getCleanlinessLevel() {
+        return cleanlinessLevel;
+    }
+
+    public void setCleanlinessLevel(CleanlinessLevel cleanlinessLevel) {
+        this.cleanlinessLevel = cleanlinessLevel;
+    }
+
+    public NoiseTolerance getNoiseTolerance() {
+        return noiseTolerance;
+    }
+
+    public void setNoiseTolerance(NoiseTolerance noiseTolerance) {
+        this.noiseTolerance = noiseTolerance;
+    }
+
+    public SocialPreference getSocialPreference() {
+        return socialPreference;
+    }
+
+    public void setSocialPreference(SocialPreference socialPreference) {
+        this.socialPreference = socialPreference;
+    }
+}
